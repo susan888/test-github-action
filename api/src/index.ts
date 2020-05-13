47ac76009ca3bd1cc00557cec10284ca20621bf9
Blank <duwalpunya8@gmail.com>
2020-05-13 11:30:24 +0545

    test1

--- api/src/index.ts
+++ api/src/index.ts
@@ -6,6 +6,7 @@ import bodyParser from 'body-parser';
 dotenv.config();
 
 const app = express();
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
