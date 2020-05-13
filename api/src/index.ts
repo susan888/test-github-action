5abac8151007cf4f4d2f9939178ecab5d3b23b33
Blank <duwalpunya8@gmail.com>
2020-05-13 10:46:48 +0545

    setup api and app

---
+++ api/src/index.ts
@@ -0,0 +1,19 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import bodyParser from 'body-parser';
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', (req: express.Request, res: express.Response) => {
+  res.json({ message: 'Dashboard page' });
+});
+
+app.listen(process.env.API_PORT, () => {
+  console.log(`Test api listening at http://localhost:${process.env.API_PORT}`);
+});
